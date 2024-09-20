06fd57264af8026094dc620f587583ea11bbbaa6
citeFred <citefred@gmail.com>
2024-09-21 00:05:19 +0900

    chore : Inject dependency of Schedule module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,13 @@ import { HttpModule } from '@nestjs/axios';
 import { MoviesService } from './movies/movies.service';
 import { MoviesController } from './movies/movies.controller';
 import { ConfigModule } from '@nestjs/config';
+import { DataCrawlerService } from './data-crawler/data-crawler.service';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ScheduleModule.forRoot(),
     HttpModule,
   ],
   controllers: [AppController, MoviesController],
